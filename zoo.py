35e18eb4dd364580257b77835a427da630cd3aed
bernauld <bdjihouessi@ivytech.edu>
2022-10-25 19:57:10 -0400

    first commit

---
+++ zoo.py
@@ -0,0 +1,5 @@
+# 1. Create a zoo.py file first. Define the hours() function, which prints the string 'Open 9-5 daily'. 
+# Then, use the interactive interpreter to import the zoo module and call its hours() function.
+
+def hours():
+    print("Open 9-5 daily")
